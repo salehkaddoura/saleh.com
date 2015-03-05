bdb4a4325adfad0adeaaeb8ff42f45a4ed474d15
salehkaddoura <saleh.kaddoura@gmail.com>
2015-03-04 18:15:04 -0800

    setting up dev and prod enviroments

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var errorHandler = require('errorhandler')
 var port = process.env.PORT || 8080;
+process.env.NODE_ENV = 'development';
 
 app.set('views', __dirname + '/app');
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
-app.use(express.static(__dirname + '/app'));
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+    app.use(express.static(__dirname + '/app'));
+    app.use(express.static(__dirname + '/.tmp'));
+}
+
+if ('production' == app.get('env')) {
+    app.use(express.static(__dirname + '/dist'));
+}
 
 app.get('/', function(req, res) {
     res.render('index');
